a1b5dfdabae825772f9b132884d8b4a7e4502d7c
Aymen idouahman <aymanidou@gmail.com>
2023-03-16 23:30:48 +0100

    0x00-hello_world update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,10 @@
 
 /**
  * main - entry point
+ *
  * description: print type size
- * return: 0
+ * 
+ * Return: 0
  */
 
 int main(void)
@@ -11,7 +13,7 @@ int main(void)
 	
 	printf("Size of a char: %d byte(s)",sizeof(char));
 	printf("Size of an int: %d byte(s)",sizeof(int));
-	printf("Size of a long int: %d byte(s)",sizeof(long int);
+	printf("Size of a long int: %d byte(s)",sizeof(long int));
 	printf("Size of a long long int : %d byte(s)",sizeof(long long int));
 	printf("Size of a float: %d byte(s)",sizeof(float));
 	return (0);
