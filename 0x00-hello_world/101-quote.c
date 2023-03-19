da7981e42e6296342e80eb866b14c07b9281897a
Aymen idouahman <aymanidou@gmail.com>
2023-03-19 21:19:41 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
- * main
+ * main - Entry
  * Description: write to console without puts or printf
  * Return: 1 
  */
 int main(void)
 {
-	char print[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, print, 60);
+	write(1, a, 60);
 	return (1);
 }
