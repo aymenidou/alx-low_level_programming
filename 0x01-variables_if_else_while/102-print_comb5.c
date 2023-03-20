6d7d27813ee391ba1a0714b1cd88066adde1539d
Aymen idouahman <aymanidou@gmail.com>
2023-03-20 15:25:34 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ *main - number comb5
+ *Description: display combination of two two-digit numbers
+ *Return: 0
+*/
+int main(void)
+{
+int n1 = 48;
+int n2 = 48;
+int n3 = 48;
+int n4 = 48;
+while (n1 <= 57)
+{
+while (n2 <= 57)
+{
+while (n3 <= 57)
+{
+while (n4 <= 57)
+{
+if (n1 != n3 & n2 != n4)
+{
+putchar(n1);
+putchar(n2);
+putchar(' ');
+putchar(n3);
+putchar(n4);
+putchar(',');
+putchar(' ');
+}
+n4++;
+}
+n3++;
+}
+n2++;
+}
+n1++;
+}
+return (0);
+}
