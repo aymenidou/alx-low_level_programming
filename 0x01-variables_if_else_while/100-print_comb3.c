9dfc61da03cd9aea4e234e32427489afb2c2c821
Aymen idouahman <aymanidou@gmail.com>
2023-03-20 14:02:39 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,30 +7,27 @@
  */
 int main(void)
 {
-
-	int n1 = 48;
-	int n2 = 48;
-
-	while (n1 <= 57)
-	{
-		n2 = 48;
-		while (n2 <= 57)
-		{
-			if (n1 != n2 && n1 < n2)
-			{
-				putchar(n1);
-				putchar(n2);
-				if (n1 != 56)
-				{
-
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			n2++;
-		}
-		n1++;
-	}
-	return (0);
+int n1 = 48;
+int n2 = 48;
+while (n1 <= 57)
+{
+n2 = 48;
+while (n2 <= 57)
+{
+if (n1 != n2 && n1 < n2)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 56)
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+n2++;
+}
+n1++;
+}
+putchar('\n');
+return (0);
+}
