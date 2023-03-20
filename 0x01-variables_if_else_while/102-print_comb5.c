a1342864c923f2e6e544a74fa7af840991ec4cce
Aymen idouahman <aymanidou@gmail.com>
2023-03-20 21:25:05 +0000

    102-print_comb5.c change logic

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,38 +7,29 @@
 */
 int main(void)
 {
-int n1 = 48;
-int n2 = 48;
-int n3 = 48;
-int n4 = 48;
-while (n1 <= 57)
+int n1 = 0;
+int n2 = 0;
+while (n1 <= 99)
 {
-n2 = 48;
-while (n2 <= 57)
+n2 = n1;
+while (n2 <= 99)
 {
-n3 = 48;
-while (n3 <= 57)
+if (n1 != n2)
 {
-n4 = 48;
-while (n4 <= 57)
-{
-if (n1 != n3 && n2 != n4)
-{
-putchar(n1);
-putchar(n2);
+putchar((n1 / 10) + 48);
+putchar((n1 % 10) + 48);
 putchar(' ');
-putchar(n3);
-putchar(n4);
+putchar((n2 / 10) + 48);
+putchar((n2 % 10) + 48);
+if (n1 != 98 || n2 != 99)
+{
 putchar(',');
 putchar(' ');
 }
-n4++;
 }
-n3++;
+++n2;
 }
-n2++;
-}
-n1++;
+++n1;
 }
 putchar('\n');
 return (0);
