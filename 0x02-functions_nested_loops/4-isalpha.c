80ffba3b4736bb72efa7cb7edeb2776f20a8ac93
Aymen idouahman <aymanidou@gmail.com>
2023-03-21 23:41:32 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _isalpha - entry
+ * @c: input character
+ * Return: 1 if lowercase or uppercase 0 if not
+ */
+int _isalpha(int c){
+
+	if ((c <= 122 && c >= 97) || (c 90<= && c >=65 ))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
