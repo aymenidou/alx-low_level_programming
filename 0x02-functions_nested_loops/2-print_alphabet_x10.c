c7d7fc06f4a92ff3f8a450673fbe4c7d95cceab0
Aymen idouahman <aymanidou@gmail.com>
2023-03-21 22:59:56 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char start = 'a';
+
+		while (start < 'z')
+		{
+			_putchar(start);
+		}
+		_putchar('\n');
+	}
+}
