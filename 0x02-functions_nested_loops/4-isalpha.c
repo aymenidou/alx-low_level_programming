6e531a55805674c0ce2193485d4559e4b1bddb55
Aymen idouahman <aymanidou@gmail.com>
2023-03-21 23:44:32 +0000

    4-isalpha.c forgot betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
-#include "main"
+#include "main.h"
 
 /**
  * _isalpha - entry
  * @c: input character
  * Return: 1 if lowercase or uppercase 0 if not
  */
-int _isalpha(int c){
+int _isalpha(int c)
+{
 
-	if ((c <= 122 && c >= 97) || (c 90<= && c >=65 ))
+	if ((c <= 122 && c >= 97) || (c <= 90 && c >= 65))
 	{
 		return (1);
 	}
