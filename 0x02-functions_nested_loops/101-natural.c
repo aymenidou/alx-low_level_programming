1327a723801351c4e31b1158169960a5ef265c71
Aymen idouahman <aymanidou@gmail.com>
2023-03-22 22:19:31 +0000

    101-natural.c initial

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print sum of multiplication of 3 or 5 below 1024
+ */
+
+void main(void)
+{
+	int sum = 0;
+
+	for (int i = 1; i<1024 ; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum += i;
+		}
+		else if (i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n",sum);
+}
