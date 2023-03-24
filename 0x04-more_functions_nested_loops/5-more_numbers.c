86198f53684dae6a224f3f4d64bf3fa3430a2d0e
Aymen idouahman <aymanidou@gmail.com>
2023-03-24 01:33:35 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry
+ * Description: print numbers
+ */
+
+void more_numbers(void)
+{
+
+	int i = 0;
+
+	while (i <= 14)
+	{
+		if (i % 10 != 0)
+		{
+			_putchar((i % 10) + 48);
+			_putchar((i / 10) + 48);
+		}
+		else
+		{
+			_putchar(i + 48);
+		}
+		i++;
+	}
+}
