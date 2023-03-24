c4900062ad706b8590be5d76ac8482a585be7fa6
Aymen idouahman <aymanidou@gmail.com>
2023-03-24 01:45:30 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,10 @@ void print_most_numbers(void)
 
 	while (i <= 9)
 	{
-		if (i == 2 || i == 4)
+		if (i != 2 && i != 4)
 		{
-			continue;
+			_putchar(i + 48);
 		}
-		_putchar(i + 48);
 		i++;
 	}
 	_putchar('\n');
