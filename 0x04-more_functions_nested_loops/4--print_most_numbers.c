ab7560b5c9b5cd4959be82a26c232cbb07e927fa
Aymen idouahman <aymanidou@gmail.com>
2023-03-24 01:25:23 +0000

    4--print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4--print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry
+ * Description: print 0 to 9 without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + 48);
+		i++;
+	}
+	_putchar('\n');
