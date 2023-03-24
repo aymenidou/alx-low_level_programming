a70a9c7ff04e8448dbbd7489d699a2b14b340c6e
Aymen idouahman <aymanidou@gmail.com>
2023-03-24 02:11:07 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,20 +10,18 @@ void more_numbers(void)
 
 	int i = 0, j = 0;
 
-	while (j < 10)
+
+	for (i = 0; i < 10; i++)
 	{
-
-		while (i <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i > 9)
+			if (j > 9)
 			{
-				_putchar((i / 10) + 48);
+				_putchar((j / 10) + '0');
 			}
-			_putchar((i % 10) + 48);
-
-			i++;
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
-		j++;
 	}
+
 }
