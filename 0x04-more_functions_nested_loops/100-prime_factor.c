72249afb8b8c49663375c425fa4d66860869adfb
Aymen idouahman <aymanidou@gmail.com>
2023-03-24 22:34:15 +0000

    100-prime_factor.c remove a space

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
- * Description: prints the largest prime factor of the number 612852475143
+ * main - prints the largest prime factor of the number 612852475143
+ * Return: 0
  */
 
 int main(void)
