4da3ef1c0422289e56f7fb51089a31f360237430
Aymen idouahman <aymanidou@gmail.com>
2023-03-27 21:58:44 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,13 +14,13 @@ void rev_string(char *s)
 	{
 		n++;
 	}
-
-	for (i = n - 1 ; i >= 0 ; i--)
+j = n - 1;
+	for (i = 0 ; i < n ; i++)
 	{
 		char a = s[i];
 		char b = s[j];
 
 		s[i] = b;
 		s[j] = a;
-		j++;
+		j--;
 	}
