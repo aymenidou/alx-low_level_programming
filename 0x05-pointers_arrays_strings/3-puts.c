6194677ab7140bbac4bdcd510b08d68af268385d
Aymen idouahman <aymanidou@gmail.com>
2023-03-27 21:37:48 +0000

    3-puts.c 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - entry point
+ * @str: input string
+ */
+
+void _puts(char *str)
+{
+puts(str);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse
+ * @s: string pointer
+ */
+
+void print_rev(char *s)
+{
+	int num = 0, i;
+
+	while (s[i] != '\n')
+	{
+		num++;
+	}
+	for (i = num - 1 ; i >= 0 ; i--)
+	{
+		printf("%c", *(s + i));
+	}
+	printf("\n");
+}
