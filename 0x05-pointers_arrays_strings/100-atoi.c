7b9dd46c5cd0348e1770a8a4f9a140187bc47a3b
Aymen idouahman <aymanidou@gmail.com>
2023-03-27 22:37:56 +0000

    additional tasks

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * atoi - entry point
+ * @s: string pointer
+ * Return integer
+ */
+
+int _atoi(char *s)
+{
+	int n, i, m;
+
+	m = 1;
+	i = n = 0;
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			m = m * -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	m = m * -1;
+	return (n * m);
+}
