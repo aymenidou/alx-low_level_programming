0e3ec5dcc0813f0dce969bd49e5414c2d5238eb5
Aymen idouahman <aymanidou@gmail.com>
2023-03-30 15:25:44 +0000

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - change all lower case letter to uppercase
+ * @str: change lowercase letters to uppercase
+ * Return: return uppercase string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
