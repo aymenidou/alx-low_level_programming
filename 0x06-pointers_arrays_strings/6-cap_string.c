ef6dab5600a458944a554f25557c1c29d57aa3f5
Aymen idouahman <aymanidou@gmail.com>
2023-03-30 19:18:20 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,11 +23,11 @@ char *cap_string(char *str)
 	while (str[i] != '\0')
 	{
 		if ((str[i] >= 'a' && str[i] <= 'z')
-		&& (str[i-1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n'
-			|| str[i-1] == ',' || str[i-1] == ';' || str[i-1] == '.'
-			|| str[i-1] == '!' || str[i-1] == '?' ||str[i-1] == '"'
-			|| str[i-1] == '(' || str[i-1] == ')' || str[i-1] == '{'
-			|| str[i-1] == '}'))
+		&& (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+			|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+			|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
+			|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+			|| str[i - 1] == '}'))
 		{
 			str[i] = str[i] - 32;
 		}
