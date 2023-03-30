ca0f1d2a4087703a2c4d91179c771352faab26ce
Aymen idouahman <aymanidou@gmail.com>
2023-03-30 23:05:46 +0000

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - print integer
+ * @n: int input
+ */
+
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		n *= 1;
+		_putchar('-');
+	}
+	if (n > 9)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + 48);
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,3 +8,5 @@ char *string_toupper(char *str);
 char *cap_string(char *s);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
