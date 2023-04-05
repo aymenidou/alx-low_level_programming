93398593e383a72e1ad4fd695666e28f4d88fc4f
Aymen idouahman <aymanidou@gmail.com>
2023-04-05 23:28:43 +0000

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - calculate factotial
+ * @n: input integer
+ * Return factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
