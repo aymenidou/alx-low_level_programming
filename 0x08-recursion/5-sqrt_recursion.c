801185b4a20a06a37590d733f7c2895a5f5a2cce
Aymen idouahman <aymanidou@gmail.com>
2023-04-06 18:38:01 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * _sqrt_recursion - square root by recursion
+ * @n: integer input
+ * Return: calculate square root
+ */
+
+int _sqrt_recursion(int n)
+{
+return (find_square(n, 1));
+}
+
+/**
+ * find_square - test numbers to find square
+ * @n: input int
+ * @m: input int
+ * Return: square root
+ */
+int find_square(int n, int m)
+{
+	int x = m * m;
+
+	if (x > n)
+	{
+		return (-1);
+	}
+	if (x == n)
+	{
+		return (m);
+	}
+	return (find_square(n, m + 1));
+}
