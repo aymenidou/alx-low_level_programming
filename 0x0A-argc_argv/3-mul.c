dcba6cc2ac7f1cb597b91e5b2e8bc1f59646c836
Aymen Idouahman <aymanidou@gmail.com>
2023-04-15 21:36:50 +0000

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -8,13 +9,16 @@
  */
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+	int x, y;
+
+	if (argc == 3)
 	{
-		printf("%d\n", argv[1] * argv[2]);
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+
+		printf("%d\n", x * y);
+		return(0);
 	}
-	else
-	{
-		printf("Error\n");
-	}
+	printf("Error\n");
 	return (1);
 }
