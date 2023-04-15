caa7d561384cd4f1dcbe089a57424d96f3b336c1
Aymen Idouahman <aymanidou@gmail.com>
2023-04-15 23:34:10 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ * @argc: args count
+ * @argv: args
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, r = 0;
+	char x;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+
+			x = atoi(argv[i][j]);
+			if (!isdigit(atoi(x)))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		r += atoi(argv[i]);
+	}
+	printf("%d\n", r);
+	return (0);
+
+}
