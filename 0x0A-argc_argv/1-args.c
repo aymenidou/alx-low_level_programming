5c4ccdae93fdc40a4b9b2c1f4fcd7b707392cee3
Aymen Idouahman <aymanidou@gmail.com>
2023-04-15 20:03:54 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: args count
+ * @argv: args
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((unused)) *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
