7b69d776b22d6dae6c6e4d9a3d45281af6fd4457
Aymen Idouahman <aymanidou@gmail.com>
2023-04-17 23:58:59 +0000

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialise var of type dog
+ * @d: input dog
+ * @name: input name
+ * @age: input age
+ * @owner: input owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
