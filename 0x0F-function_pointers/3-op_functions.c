1d16fbeea9a5e8e2f63b0108c161da96a753f11d
Aymen Idouahman <aymanidou@gmail.com>
2023-04-21 22:07:33 +0000

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * op_add - add operation
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - difference operation
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: difference of @a and @b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiple operation
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: product of @a and @b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides operation
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: results of the division @a and @b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - gets the reminder of the division between @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: the remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
