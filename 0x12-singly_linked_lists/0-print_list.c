1c0efa2d34d3c21b7562013c2165a3a7f8987a7b
Aymen Idouahman <aymanidou@gmail.com>
2023-04-27 23:13:45 +0100

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - entry point
+ * @h: input head of the linked list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;	
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
