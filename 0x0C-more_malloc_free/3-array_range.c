04ceb752518a8f03510394b544f899925be99efa
Aymen Idouahman <aymanidou@gmail.com>
2023-05-01 23:24:03 +0100

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *array_range - entry point
+ * @min: input int
+ * @max: input int
+ * Return: the pointer to the newly created array
+ */
+int *array_range(int min, int max)
+{
+	int *p, i = 0;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	p = malloc((max - min + 1) * sizeof(int));
+	if (ptr)
+	{
+		while (min <= max)
+		{
+			p[i] = min;
+			min++;
+		}
+	}
+	return (ptr);
+}
