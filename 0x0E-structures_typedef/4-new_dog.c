165c5fb82f2b3d034f2ca86d8cc17e11d7ef44d4
Aymen Idouahman <aymanidou@gmail.com>
2023-05-01 15:02:20 +0100

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,67 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - entry point
+ * @s: input string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+
+/**
+ * new_dog - create new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int idx;
+	dog_t *d;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+
+	d = malloc(sizeof(dog_t));
+	if (d)
+	{
+		idx = _strlen(name);
+		d->name = malloc(idx * sizeof(char));
+		if (d->name == NULL) 
+		{
+			free(d);
+			return (NULL);
+		}
+		for (idx = 0; name[idx] != '\0'; idx++)
+		{
+			d->name[idx] = name[idx];
+		}
+		d->age = age;
+		idx = _strlen(owner);
+		d->owner = malloc(idx * sizeof(char));
+		if (d->owner == NULL)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+		for (idx = 0; owner[idx] != '\0'; idx++)
+		{
+			d->owner[idx] = owner[idx];
+		}
+	}
+	return (d);
+}
