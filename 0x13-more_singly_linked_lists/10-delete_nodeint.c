201f08dc38bfd23a0bc7247bedb3d1318c908042
Aymen Idouahman <aymanidou@gmail.com>
2023-05-02 23:30:03 +0100

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *tmp, *del;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	tmp = *head;
 
