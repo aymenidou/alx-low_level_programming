0e999acb7d25731b212af00173f71ddbbfcb9ab0
Aymen Idouahman <aymanidou@gmail.com>
2023-05-09 00:18:23 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,14 +22,14 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (count = 0; text_content[count]; count++)
-		       ;
-	}	
+		for (count = 0; text_content[count];)
+			count++;
+	}
 	wr = write(op, text_content, count);
 	if (wr == -1)
 	{
 		return (-1);
 	}
-	close (op);
+	close(op);
 	return (-1);
 }
