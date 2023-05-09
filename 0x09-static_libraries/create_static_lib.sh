eefb42683efeb4e4be8e2ef18cbb192e96440f00
Aymen Idouahman <aymanidou@gmail.com>
2023-05-09 19:13:24 +0100

    delete past projects for deferment

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# create a static library called liball.a from all the .c files
-gcc -c *.c
-ar rcs liball.a *.o
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/** use ternary condition to check if input is positive of negative */
-#define ABS(x) (((x) >= 0) ? (x) : -(x))
-
-#endif
