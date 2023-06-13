2f1eb23f061b2ec7a497b7f398b408c406bcee05
Aymen Idouahman <aymanidou@gmail.com>
2023-06-13 14:29:00 +0100

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ * @c: input string
+ * Return: return string length
+ */
+
+int _strlen(char *c)
+{
+        int i = 0;
+
+        while (c[i] != '\0')
+        {
+                i++;
+        }
+        return (i);
+}
+
+
+/**
+ * str_concat - entry point
+ * @s1: input character
+ * @s2: input character
+ * Return: pointer to concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int stringSize, i, j;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	stringSize = _strlen(s1);
+	stringSize += _strlen(s2);
+	p = malloc(sizeof(char) * (stringSize + 1));
+	if (p)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			p[i] = s1[i];
+		}
+		for (j = 0; s2[j] != '\0'; j++ ,i++)
+		{
+			p[i] = s2[j];
+		}
+		p[i] = '\0';
+	}
+	return (p);
+}
