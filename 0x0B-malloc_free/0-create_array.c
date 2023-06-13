1f1091bedb08084cfafd6ff136164befdc8303af
Aymen Idouahman <aymanidou@gmail.com>
2023-06-13 11:44:24 +0100

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+ * *create_array - entry point
+ * @size: array size
+ * @c: character
+ * Return: initialized array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+	
+	if (size == 0)
+	{
+		return NULL;
+	}
+	array = malloc(size);
+	if (array)
+	{
+		for (i=0;i<size;i++)
+		{
+			array[i] = c;
+		}
+	}
+	return array;
+
+}
