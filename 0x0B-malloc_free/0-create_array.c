85c004f3a99a1732e1933a02503a5e74f8c5d385
Aymen Idouahman <aymanidou@gmail.com>
2023-06-13 11:46:22 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,19 +10,19 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	array = malloc(size);
 	if (array)
 	{
-		for (i=0;i<size;i++)
+		for (i = 0 ; i < size ; i++)
 		{
 			array[i] = c;
 		}
 	}
-	return array;
+	return (array);
 
 }
