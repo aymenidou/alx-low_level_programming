d6eb513d47b6de1bb9c3ec2950d0684b83a736b0
Aymen Idouahman <aymanidou@gmail.com>
2023-07-01 01:18:56 +0100

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - list length
+ * @h: list head
+ * Return: number of elements is the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+	*tmp = *h;
+
+	while (tmp)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	return (i);
+
+}
