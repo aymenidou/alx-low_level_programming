c16b189c056cb35af675e6dcd1277a6a0f038c74
Aymen Idouahman <aymanidou@gmail.com>
2023-07-01 01:07:56 +0100

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * printf_list - print all elements of a list_t
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+	if (h)
+	{
+		*tmp = *h;
+	if (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->str)
+				printf("[%i] %s\n",tmp->len,tmp->str);
+			else
+				printf("[0] (nil)\n");
+			tmp = tmp->next;
+			i++;
+		}
+	}
+	}
+	return (i);
+}
