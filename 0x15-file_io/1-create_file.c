f2eb02966b5f2eb46bf8733ead43f1d3687434c8
Aymen Idouahman <aymanidou@gmail.com>
2023-07-10 23:49:18 +0100

    0x15-file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - entry point
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr, count = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (op == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (count = 0; text_content[count];)
+			count++;
+	}
+	wr = write(op, text_content, count);
+	if (wr == -1)
+	{
+		return (-1);
+	}
+	close(op);
+	return (1);
+}
