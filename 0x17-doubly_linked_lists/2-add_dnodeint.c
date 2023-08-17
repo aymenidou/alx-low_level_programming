da54d656fc970ac3864dcd6ae7bb7a7bcbd66c29
Aymen Idouahman <aymanidou@gmail.com>
2023-08-17 21:54:19 +0100

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -5,14 +5,19 @@
  * @head: list head
  * @n: integer input
  * Return: pointer to new element
-*/
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node){
+	if (new_node)
+	{
 		new_node->n = n;
+		if (!head)
+		{
+			return (new_node);
+		}
 		new_node->next = *head;
 		new_node->prev = NULL;
 		if (*head)
