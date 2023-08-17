d4765e37759b002850d025d26c281fe725800b08
Aymen Idouahman <aymanidou@gmail.com>
2023-08-17 21:58:31 +0100

    3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add at the end
+ * @head: list head
+ * @n: integer input
+ * Return: pointer to new element
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *end = malloc(sizeof(dlistint_t)), *tmp;
+
+	if (end)
+	{
+		end->n = n;
+		end->next = NULL;
+		if (*head == NULL)
+		{
+			*head = end;
+			return (end);
+		}
+		tmp = *head;
+		while (tmp->next)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = end;
+	}
+	return (end);
+}
