40a39451033d563c9210b467c2cbe32eebdbdc0e
Aymen Idouahman <aymanidou@gmail.com>
2023-08-17 22:08:43 +0100

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free list
+ * @head: list head
+*/
+void free_dlistint(dlistint_t *head)
+{
+	if (!head)
+		return;
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
